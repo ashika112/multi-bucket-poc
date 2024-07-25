bf78b4b58ff325e10584d85692b2340cdc473546
ashika112 <akasivis@amazon.com>
2024-07-25 15:46:30 -0700

    multibucket support

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,11 @@ import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import { copy, uploadData } from "aws-amplify/storage";
 
-// Amplify.configure({
-//   Auth: {
-//     Cognito: {
-//       identityPoolId: "us-west-2:478c85b4-4406-44aa-a3c9-2121d85920f7",
-//       allowGuestAccess: true,
-//     },
-//   },
-//   Storage: { S3: { bucket: "bucket" } },
-// });
-
 Amplify.configure(outputs);
+console.log(Amplify.getConfig());
 
 function App() {
   const [status, setStatus] = useState<string | null>(null);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [bucket, setBucket] = useState<any>({});
 
   return (
     <>
@@ -28,12 +17,18 @@ function App() {
       <div className="card">
         <button
           onClick={() => {
-            const task = uploadData({
-              path: "media/abc.txt",
-              data: "hello world",
-              options: { bucket: "multi-bucket-3", region: "us-west-2" },
-            });
-            task.result.then((res) => setStatus(res.path));
+            try {
+              const task = uploadData({
+                path: "media/abc1.txt",
+                data: "hello world",
+                options: {
+                  bucket: { bucketName: "bucket-2", region: "us-west-2" },
+                },
+              });
+              task.result.then((res: any) => setStatus(res.path));
+            } catch (e) {
+              console.log("amplify", e);
+            }
           }}
         >
           Upload
@@ -41,27 +36,26 @@ function App() {
 
         <button
           onClick={async () => {
-            const res = await copy({
-              source: { path: "media/abc.txt" },
-              destination: {
-                path: "media/abcCOpy.txt",
-                bucket: { name: "multi-bucket-2", region: "us-east-2" },
-              },
-            });
-            console.log("copy", res);
+            try {
+              const res = await copy({
+                source: {
+                  path: "media/abc.txt",
+                  bucket: "default",
+                },
+                destination: {
+                  path: "media/abcCOpy-1.txt",
+                  bucket: "default-1",
+                },
+              });
+              console.log("copy", res);
+            } catch (e) {
+              // const error: StorageError = e;
+              console.log(e);
+            }
           }}
         >
           copy
         </button>
-        {/* <button
-          onClick={() => {
-            const b = getBuckets();
-            setBucket(b[1]);
-            console.log(b);
-          }}
-        >
-          Get Buckets
-        </button> */}
         <p>{status}</p>
       </div>
     </>
